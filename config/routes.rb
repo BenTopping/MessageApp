af431b626db8e2b28db46d15950cf0239c2da2ab
Benjamin Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-10-29 11:44:53 +0000

    Authentication update
    Add authentication for users which error messages, add local chatroom (messages but without websocket)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'chatroom#index'
 
+  get 'signup', to: 'users#new'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  post 'message', to: 'messages#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
